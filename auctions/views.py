052f27bf75083717b6a13cf7acd53a94992565e5
ca-Stella <stella.2532@gmail.com>
2022-09-30 22:48:24 -0600

    Set up individual listing pages

--- auctions/views.py
+++ auctions/views.py
@@ -85,3 +85,9 @@ def create(request):
         return render(request, "auctions/create.html", {
             "form": AuctionForm()
         })
+
+def listing(request, listing_id):
+    listing = Auction.objects.get(pk = listing_id)
+    return render(request, "auctions/listing.html", {
+        "listing": listing
+    })
